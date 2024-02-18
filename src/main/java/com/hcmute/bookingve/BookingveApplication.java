41b0a87dc7f99d1d180c8f207c7df1e68b0264e2
Phạm Quốc Hưng <96472052+quochung2k3@users.noreply.github.com>
2024-02-18 22:09:41 +0700

    Initial commit

---
+++ src/main/java/com/hcmute/bookingve/BookingveApplication.java
@@ -0,0 +1,13 @@
+package com.hcmute.bookingve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingveApplication.class, args);
+	}
+
+}
