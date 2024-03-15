354cfd75c2f90a4d7415b5119cbcc79d893fa14e
Phạm Quốc Hưng <96472052+quochung2k3@users.noreply.github.com>
2024-03-15 11:04:41 +0700

    Update project

---
+++ src/main/java/com/hcmute/bookingve/Controller/Admin/AdminController.java
@@ -0,0 +1,12 @@
+package com.hcmute.bookingve.Controller.Admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {
+    @RequestMapping("/admin")
+    public String admin() {
+        return "admin/index";
+    }
+}
