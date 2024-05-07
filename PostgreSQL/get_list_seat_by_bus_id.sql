5521fe752047c5a03a10bf2921e0e63e6686dd7f
Phạm Quốc Hưng <96472052+quochung2k3@users.noreply.github.com>
2024-05-07 18:03:53 +0700

    Update

--- PostgreSQL/get_list_seat_by_bus_id.sql
+++ PostgreSQL/get_list_seat_by_bus_id.sql
@@ -8,21 +8,24 @@ CREATE OR REPLACE FUNCTION get_list_seat_by_bus_id
 )
 RETURNS TABLE(
 	"SeatId" INT,
-	"SeatName" VARCHAR,
-	"Price" INT
+	"SeatName" VARCHAR
 ) AS $BODY$
 DECLARE 
-	v_title_ids INT[];
+	
 BEGIN
 
 	RETURN QUERY
 	SELECT
-		s."SeatId",
-		s."SeatName",
-		st."Price"
-	FROM "Bus" b INNER JOIN "Seat" s ON b."BusTypeId" = s."BusTypeId"
-	INNER JOIN "SeatType" st ON s."SeatTypeId" = st."SeatTypeId"
-	WHERE b."BusId" = p_bus_id;
+		S."SeatId",
+		S."SeatName"
+	FROM "Bus" B INNER JOIN "Seat" S ON B."BusTypeId" = S."BusTypeId"
+	WHERE B."BusId" = p_bus_id
+	AND S."SeatId" NOT IN (
+		SELECT UNNEST("ListSeatId")
+		FROM "Invoice" I INNER JOIN "Reservation" R ON I."ReservationId" = R."ReservationId"
+		WHERE "IsPayed" = TRUE
+		AND R."BusId" = p_bus_id
+	);
 	
 END;
 $BODY$
