3f111f9424e693a2d04307e3c8d546e0ce00b0ac
Phạm Quốc Hưng <96472052+quochung2k3@users.noreply.github.com>
2024-05-07 19:54:19 +0700

    Update

--- src/main/java/com/hcmute/bookingve/Controller/User/PaymentController.java
+++ src/main/java/com/hcmute/bookingve/Controller/User/PaymentController.java
@@ -2,11 +2,13 @@ package com.hcmute.bookingve.Controller.User;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PaymentController {
     @RequestMapping("/paymentPage")
-    public String paymentPage() {
+    public String paymentPage(@RequestParam("busId") int busId) {
+        System.out.println(busId);
         return "user/paymentpage";
     }
 }
