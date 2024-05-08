d62bc5eef5139319f3057253349cc958220d9bf5
Phạm Quốc Hưng <96472052+quochung2k3@users.noreply.github.com>
2024-05-08 14:14:31 +0700

    Update API

--- PostgreSQL/get_list_seat_by_bus_id.sql
+++ PostgreSQL/get_list_seat_by_bus_id.sql
@@ -2,31 +2,34 @@
 	Hưng Phạm
 	SELECT * FROM get_list_seat_by_bus_id(1)
 */
+-- DROP FUNCTION get_list_seat_by_bus_id
 CREATE OR REPLACE FUNCTION get_list_seat_by_bus_id
 (
-	p_bus_id INT
+    p_bus_id INT
 )
 RETURNS TABLE(
-	"SeatId" INT,
-	"SeatName" VARCHAR
+    "SeatId" INT,
+    "SeatName" VARCHAR,
+    "Check" BOOL
 ) AS $BODY$
-DECLARE 
-	
 BEGIN
-
-	RETURN QUERY
-	SELECT
-		S."SeatId",
-		S."SeatName"
-	FROM "Bus" B INNER JOIN "Seat" S ON B."BusTypeId" = S."BusTypeId"
-	WHERE B."BusId" = p_bus_id
-	AND S."SeatId" NOT IN (
-		SELECT UNNEST("ListSeatId")
-		FROM "Invoice" I INNER JOIN "Reservation" R ON I."ReservationId" = R."ReservationId"
-		WHERE "IsPayed" = TRUE
-		AND R."BusId" = p_bus_id
-	);
-	
+    RETURN QUERY
+    SELECT
+        S."SeatId",
+        S."SeatName",
+        CASE
+            WHEN S."SeatId" IN (
+                SELECT UNNEST("ListSeatId")
+                FROM "Invoice" I 
+                INNER JOIN "Reservation" R ON I."ReservationId" = R."ReservationId"
+                WHERE "IsPayed" = TRUE
+                AND R."BusId" = p_bus_id
+            ) THEN TRUE
+            ELSE FALSE
+        END AS "Check"
+    FROM "Bus" B 
+    INNER JOIN "Seat" S ON B."BusTypeId" = S."BusTypeId"
+    WHERE B."BusId" = p_bus_id;
 END;
 $BODY$
-LANGUAGE plpgsql VOLATILE
+LANGUAGE plpgsql VOLATILE;
