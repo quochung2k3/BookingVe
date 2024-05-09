8c4f462240f7c16aead58d918046a12de056f080
Phạm Quốc Hưng <96472052+quochung2k3@users.noreply.github.com>
2024-05-09 14:31:10 +0700

    Update

--- src/main/resources/static/assets/js/paymentscript.js
+++ src/main/resources/static/assets/js/paymentscript.js
@@ -66,3 +66,36 @@ function AddHeight(){
     var element = document.querySelector(".ant-drawer-mask");
     element.style.height = pageHeight + "px";
 }
+
+var listSeat = [];
+function changeRectColor(classList, money) {
+    var newClass = classList[classList.length - 1];
+    var elementWithNewClass = document.querySelector('.' + newClass);
+    var rects = elementWithNewClass.querySelectorAll('rect');
+
+    rects.forEach(function(rect) {
+        var currentFill = rect.getAttribute('fill');
+        var currentStroke = rect.getAttribute('stroke');
+
+        if (currentFill === '#8BE5B0' && currentStroke === '#27AE60') {
+            rect.setAttribute('fill', '#FFF');
+            rect.setAttribute('stroke', '#B8B8B8');
+        }
+        else {
+            rect.setAttribute('fill', '#8BE5B0');
+            rect.setAttribute('stroke', '#27AE60');
+        }
+    });
+    var index = listSeat.indexOf(newClass);
+    if(index === -1) {
+        listSeat.push(newClass);
+    }
+    else {
+        listSeat.splice(index, 1);
+    }
+    var totalPriceText = document.querySelector('.total-price-text').textContent;
+    var total = totalPriceText.replace(/\D/g, '');
+    var totalText = listSeat.length * parseInt(total);
+    document.getElementById('list-seat-name').innerHTML = "Số ghế: {" + listSeat.toString() + "}";
+    document.getElementById('total').innerHTML = totalText + ' VNĐ';
+}
