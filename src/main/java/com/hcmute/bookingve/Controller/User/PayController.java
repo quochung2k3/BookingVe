81fd0c1d7044a990239fd0aba6d336ca0cc0ab19
Phạm Quốc Hưng <96472052+quochung2k3@users.noreply.github.com>
2024-05-10 17:29:17 +0700

    .

--- src/main/java/com/hcmute/bookingve/Controller/User/PayController.java
+++ src/main/java/com/hcmute/bookingve/Controller/User/PayController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class PayController {
     @GetMapping("/payPage")
     public String payPage() {
-        return "user/payPage";
+        return "user/pay";
     }
 }
