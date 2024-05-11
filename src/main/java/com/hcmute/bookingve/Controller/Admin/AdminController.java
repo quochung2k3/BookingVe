c0bfac0cc06d97873388b2e0e10278534851d5f9
phaminhieu30 <151518508+phaminhieu30@users.noreply.github.com>
2024-05-11 11:26:10 +0700

    FE

--- src/main/java/com/hcmute/bookingve/Controller/Admin/AdminController.java
+++ src/main/java/com/hcmute/bookingve/Controller/Admin/AdminController.java
@@ -5,14 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/admin")
 public class AdminController {
-    @GetMapping
+    @GetMapping("/abc")
     public String index() {
-        return "redirect:/admin/";
-    }
-    @RequestMapping("/")
-    public String admin() {
-        return "admin/index";
+        return "admin/fragments/menu";
     }
 }
