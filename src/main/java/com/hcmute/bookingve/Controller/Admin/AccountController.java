33be1d15eea4fa563805b2c2f9c51480814d3bc1
phaminhieu30 <151518508+phaminhieu30@users.noreply.github.com>
2024-05-11 18:15:38 +0700

    aaa

--- src/main/java/com/hcmute/bookingve/Controller/Admin/AccountController.java
+++ src/main/java/com/hcmute/bookingve/Controller/Admin/AccountController.java
@@ -1,14 +1,12 @@
 package com.hcmute.bookingve.Controller.Admin;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class AdminController {
-    @GetMapping("/abc")
-    public String index() {
-        return "admin/index";
-    }
+@RequestMapping("/admin-ql-account")
+public class AccountController {
+    @GetMapping
+    public String index () {return "admin/account/ql-account";}
 
 }
