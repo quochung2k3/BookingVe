6dc8ba5784d110b6de67c8bad2892e3e2f624e99
Phạm Quốc Hưng <96472052+quochung2k3@users.noreply.github.com>
2024-05-12 16:14:50 +0700

    Update

--- src/main/java/com/hcmute/bookingve/Controller/User/DetailBookingController.java
+++ src/main/java/com/hcmute/bookingve/Controller/User/DetailBookingController.java
@@ -5,6 +5,7 @@ import com.hcmute.bookingve.Models.Invoice;
 import com.hcmute.bookingve.Models.Reservation;
 import com.hcmute.bookingve.service.BusService;
 import com.hcmute.bookingve.service.InvoiceService;
+import com.hcmute.bookingve.service.PickUpAndDropOffService;
 import com.hcmute.bookingve.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,16 +18,17 @@ public class DetailBookingController {
     @Autowired
     InvoiceService invoiceService;
     @Autowired
-    ReservationService reservationService;
-    @Autowired
-    BusService busService;
+    PickUpAndDropOffService pickUpAndDropOffService;
     @GetMapping("/DetailBooking")
     public String detailBooking(Model model,
-                                @RequestParam("busName") String cardNumber) {
+                                @RequestParam("cardNumber") String cardNumber) {
         Invoice invoice = invoiceService.findByCardNumber(cardNumber);
-//        Reservation reservation = reservationService.findById(invoice.getReservationId());
-//        Bus bus = busService.findById(reservation.getBusId());
-//        model.addAttribute("invoice", invoice);
+        String pickUpName = pickUpAndDropOffService.findPlaceNameById(invoice.getReservation().getPickUpId());
+        String dropOffName = pickUpAndDropOffService.findPlaceNameById(invoice.getReservation().getDropOffId());
+        model.addAttribute("invoice", invoice);
+        model.addAttribute("pickUpName", pickUpName);
+        model.addAttribute("dropOffName", dropOffName);
+        System.out.println(invoice.getReservation().getBus().getVehicle().getVehicleName());
         return "user/DetailBooking";
     }
 }
