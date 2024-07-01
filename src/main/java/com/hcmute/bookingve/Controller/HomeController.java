ab9c18cc1942829a0b4191d2ca0e4117090dc6e4
Phạm Quốc Hưng <96472052+quochung2k3@users.noreply.github.com>
2024-07-01 09:21:45 +0700

    Update HomeController.java

--- src/main/java/com/hcmute/bookingve/Controller/HomeController.java
+++ src/main/java/com/hcmute/bookingve/Controller/HomeController.java
@@ -1,10 +1,13 @@
 package com.hcmute.bookingve.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-@Controller
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
-    @RequestMapping("home")
+    @GetMapping("/home")
     public String index() {
         return "index";
     }
